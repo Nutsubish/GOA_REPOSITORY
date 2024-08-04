9f8edd8f8b3a61a34f4ef8a5db2754e1f2563e25
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2024-08-04 22:15:44 +0400

    a

---
+++ Day114/classwork/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+
+
+  return (
+    <>
+      <p>code academy</p>
+    </>
+  )
+}
+
+export default App
---
+++ Day114/homework/src/App.jsx
@@ -0,0 +1,13 @@
+import Contact from "./Contact.jsx"
+
+function App() {
+
+
+  return (
+    <>
+      <Contact/>
+    </>
+  )
+}
+
+export default App
